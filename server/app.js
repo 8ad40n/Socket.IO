e36a71fbc175182477590b0fc940f9ec022d688d
8ad40n <joy184110@gmail.com>
2024-10-03 02:15:36 +0600

    user connection

--- server/app.js
+++ server/app.js
@@ -1,19 +1,36 @@
-const express = require('express')
-const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const { createServer } = require('http');
 const { Server } = require('socket.io');
-const app = express()
-const port = 3000
 
-const App = http.createServer(app);
-const server = new Server(App);
+const app = express();
+const port = 3000;
 
-const io = new Server(server);
+const server = createServer(app);
+
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
+io.on("connection", (socket) => {
+  console.log("User connected");
+  console.log("ID", socket.id);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+// Start the HTTP server
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
